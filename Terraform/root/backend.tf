d7da8081f4769bdcb302cbad65a6c1997958fd04
Matthew Igbinehi <matthewesosa@gmail.com>
2023-10-06 21:43:29 +0200

    updated s3 and dynamoDB remote backend

--- Terraform/root/backend.tf
+++ Terraform/root/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "tfstate-matthew-101"
-    key    = "backend/research_project-iac.tfstate"
+    bucket = "tfstate-matthew-01"
+    key    = "backend/iac-research_project.tfstate"
     region = "us-east-1"
     dynamodb_table = "research_project-remote-backend"
   }
