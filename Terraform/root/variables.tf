8f60b29520290f4d62e1f3feb9977674aa248096
Matthew Igbinehi <matthewesosa@gmail.com>
2023-10-06 23:15:37 +0200

    added subnet variables for root

--- Terraform/root/variables.tf
+++ Terraform/root/variables.tf
@@ -1,3 +1,9 @@
 variable region {}
 variable project_name {}
 variable vpc_cidr {}
+variable pub_sub_1a_cidr {}
+variable pub_sub_2b_cidr {}
+variable priv_sub_3a_cidr {}
+variable priv_sub_4b_cidr {}
+variable db_username {}
+variable db_password {}
