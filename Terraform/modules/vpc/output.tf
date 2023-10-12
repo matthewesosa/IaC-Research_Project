d2e8bf39e3814d5a8ac3b7a506bab7ff22215cba
Matthew Igbinehi <matthewesosa@gmail.com>
2023-10-12 17:43:47 +0200

    added vpc_id and region outputs

--- Terraform/modules/vpc/output.tf
+++ Terraform/modules/vpc/output.tf
@@ -1,3 +1,17 @@
+
+output "project_name" {
+  value = var.project_name
+}
+
+output "region" {
+  value = var.region
+}
+
+output "vpc_id" {
+  value = aws_vpc.vpc.id
+}
+
+
 output "pub_sub_1a_id" {
   value = aws_subnet.pub_sub_1a.id
 }
