a9fafe6033521aa556ccf082f30f6f3a6c40e65d
Matthew Igbinehi <matthewesosa@gmail.com>
2023-10-12 16:53:06 +0200

    corrected s3  backed bucket name

--- Terraform/root/backend.tf
+++ Terraform/root/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "iac-webappBucket"
+    bucket = "iac-webappbucket"
     key    = "backend/iac-researchproject.tfstate"
     region = "eu-central-1"
     dynamodb_table = "iac-researchproject-remote-backend"
