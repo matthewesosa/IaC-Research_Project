2e93e8dd13315e1df49655ea832f1444617eb69d
Matthew Igbinehi <matthewesosa@gmail.com>
2023-10-19 19:38:10 +0200

    added new profile

--- Cloudformation/create.sh
+++ Cloudformation/create.sh
@@ -1,1 +1,1 @@
-aws cloudformation create-stack --stack-name $1 --template-body file://$2 --parameters file://$3 --profile 226915686048_PowerUserAccess --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"
+aws cloudformation create-stack --stack-name $1 --template-body file://$2 --parameters file://$3 --profile mypassrole --capabilities "CAPABILITY_IAM" "CAPABILITY_NAMED_IAM"
