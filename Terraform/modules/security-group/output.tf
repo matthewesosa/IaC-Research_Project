5557628156949fb98c5543228c17513d64304e76
Matthew Igbinehi <matthewesosa@gmail.com>
2023-10-26 17:47:10 +0200

    defined output for security-group module

--- Terraform/modules/security-group/output.tf
+++ Terraform/modules/security-group/output.tf
@@ -3,8 +3,8 @@ output "alb_sg_id" {
   value = aws_security_group.alb_sg.id
 }
 
-output "webServer_sg_id" {
-  value = aws_security_group.webServer_sg.id
+output "webserver_sg_id" {
+  value = aws_security_group.webserver_sg.id
 }
 
 output "mydb_sg_id" {
