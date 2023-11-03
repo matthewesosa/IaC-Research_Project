47904aee70643b867f682eba91c407212d115754
Matthew Igbinehi <matthewesosa@gmail.com>
2023-11-03 11:05:03 +0100

    updated mydb name

--- Terraform/modules/database/variables.tf
+++ Terraform/modules/database/variables.tf
@@ -4,5 +4,5 @@ variable "priv_sub_4b_id" {}
 variable "mydb_username" {}
 variable "mydb_password" {}
 variable "mydb_sub_name" {
-    default = "test-db-subnet-a-b"
+    default = "mydb-subnet-a-b"
 }
