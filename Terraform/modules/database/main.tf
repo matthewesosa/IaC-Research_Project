018ee4fa680407bdc0ec6c30a80748ae83d4f1ee
Matthew Igbinehi <matthewesosa@gmail.com>
2023-11-03 11:01:38 +0100

    added subnet dependecy for mydb to define deletion sequence for terraform

--- Terraform/modules/database/main.tf
+++ Terraform/modules/database/main.tf
@@ -1,10 +1,10 @@
 resource "aws_db_subnet_group" "mydb-subnet" {
   name       = var.mydb_sub_name
-  subnet_ids = [var.priv_sub_3a_id, var.priv_sub_4b_id] #private subnet ids here
+  subnet_ids = [var.priv_sub_3a_id, var.priv_sub_4b_id]  #private subnet ids here
 }
 
 resource "aws_db_instance" "mydb" {
-  identifier              = "testdb-instance"
+  identifier              = "iac-testdb-instance"
   engine                  = "mysql"
   instance_class          = "db.t2.small"
   allocated_storage       = 10
@@ -17,8 +17,9 @@ resource "aws_db_instance" "mydb" {
   vpc_security_group_ids = [var.mydb_sg_id] # Replace with your desired security group ID
 
   db_subnet_group_name = aws_db_subnet_group.mydb-subnet.name
+  depends_on           = [aws_db_subnet_group.mydb-subnet]
 
   tags = {
-    Name = "mydb-test"
+    Name = "iac-mydb"
   }
 }
